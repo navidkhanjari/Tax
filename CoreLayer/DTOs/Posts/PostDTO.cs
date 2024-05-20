fcbf121fb7cbaa2a50d6599f6d31c5d3a79eae1e
navid khanjari <navidkhanjari72@gmail.com>
2024-05-20 11:05:13 +0330

    Compelete Post Views

--- CoreLayer/DTOs/Posts/PostDTO.cs
+++ CoreLayer/DTOs/Posts/PostDTO.cs
@@ -1,17 +1,18 @@
 ﻿namespace CoreLayer.DTOs.Posts
 {
-	public class PostDTO
-	{
-		public int Id { get; set; }
-		public string Title { get; set; }
-		public string ShortDescription { get; set; }
-		public string Description { get; set; }
-		public string Authour { get; set; }
-		public string ImageName { get; set; }
-		public bool Visible { get; set; }
-		public string Slug { get; set; }
-		public string MetaTitle { get; set; }
-		public string MetaDescription { get; set; }
+    public class PostDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Authour { get; set; }
+        public string ImageName { get; set; }
+        public bool Visible { get; set; }
+        public string Slug { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
         public string MetaKeyWords { get; set; }
-	}
+        public DateTime PublishDate { get; set; }
+    }
 }
