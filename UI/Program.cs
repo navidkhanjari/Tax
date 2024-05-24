0ad720d10d4f71e8527efd63349cc32da52dc7ee
navidkhanjari <navidkhanjari72@gmail.com>
2024-05-24 16:28:21 +0330

    Change Font

--- UI/Program.cs
+++ UI/Program.cs
@@ -55,16 +55,12 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
-
+app.UseStatusCodePagesWithReExecute("/ErrorHandler/{0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
