7be54d9801f20bc831ec5fdaa34efbcdecb6efb7
navidkhanjari <navidkhanjari72@gmail.com>
2024-06-16 21:00:05 +0330

    Fix Some edit

--- UI/wwwroot/site/Js/Custom.js
+++ UI/wwwroot/site/Js/Custom.js
@@ -381,24 +381,24 @@ $(document).ready(function () {
 
 
 
-//popupgallery
-$('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-with-zoom mfp-img-mobile',
-    gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0, 1]
-    },
-    zoom: {
-        enabled: true,
-        duration: 300
-    },
-    image: {
-        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-    }
-});
+////popupgallery
+//$('.popup-gallery').magnificPopup({
+//    delegate: 'a',
+//    type: 'image',
+//    tLoading: 'Loading image #%curr%...',
+//    mainClass: 'mfp-with-zoom mfp-img-mobile',
+//    gallery: {
+//        enabled: true,
+//        navigateByImgClick: true,
+//        preload: [0, 1]
+//    },
+//    zoom: {
+//        enabled: true,
+//        duration: 300
+//    },
+//    image: {
+//        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+//    }
+//});
 
 
